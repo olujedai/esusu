51099cdc84d4fae8606b64efb8d2d0cd5d59c4f4
olujedai <olujedai@gmail.com>
2019-07-31 17:09:22 +0100

    Added new Sudo permission, prevented unauthourized access to all users. fixed error that prevented users from being invited

--- esusu/api/permissions.py
+++ esusu/api/permissions.py
@@ -1,19 +1,27 @@
 from rest_framework import permissions
 
 class IsAuthenticatedNotPost(permissions.IsAuthenticated):
-    def has_permission(self, request, view):
-        if request.method == 'POST':
-            return True
-        return super(IsAuthenticatedNotPost, self).has_permission(request, view)
+	def has_permission(self, request, view):
+		if request.method == 'POST':
+			return True
+		return super(IsAuthenticatedNotPost, self).has_permission(request, view)
 
 class IsASocietyAdmin(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_society_admin
+	def has_permission(self, request, view):
+		return request.user.is_society_admin
 
 class IsInASociety(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return bool(request.user.society)
+	def has_permission(self, request, view):
+		return bool(request.user.society)
 
 class IsNotInASociety(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return not bool(request.user.society)
+	def has_permission(self, request, view):
+		return not bool(request.user.society)
+
+class IsSudoUser(permissions.BasePermission):
+	"""
+	Allow only POST requests.
+	"""
+
+	def has_object_permission(self, request, view, obj):
+		return request.user.is_superuser
