7b4d6ed9a1185b5bbafdecb051c9908c176595b6
olujedai <olujedai@gmail.com>
2019-08-01 00:49:27 +0100

    Added endpoint for viewing the society along with the tenuactive tenure

--- esusu/api/serializers/__init__.py
+++ esusu/api/serializers/__init__.py
@@ -1,6 +1,6 @@
 from .collection_schedule import CollectionScheduleSerializer
 from .credit import CreditSerializer
-from .society import SocietySerializer, SocietyUserSerializer, SocietyDetailsSerializer
+from .society import SocietySerializer, SocietyUserSerializer, SocietyTenureSerializer
 from .society_account import SocietyAccountSerializer
 from .tenure import TenureSerializer
 from .user import (BaseUserSerializer, UserInvitationSerializer, UserContributionsSerializer,
--- esusu/api/serializers/society.py
+++ esusu/api/serializers/society.py
@@ -1,24 +1,33 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from ..models import Society
 from .society_account import SocietyAccountSerializer
 from .user import UserContributionsSerializer
+from .tenure import TenureSerializer
+import arrow
 
-class SocietySerializer(ModelSerializer):
-    account = SocietyAccountSerializer(read_only=True)
-    class Meta:
-        model = Society
-        fields = '__all__'
 
-    def save(self):
-        self.validated_data['creator'] = self.context['request'].user
-        super().save()
+class SocietySerializer(serializers.ModelSerializer):
+	account = SocietyAccountSerializer(read_only=True)
+	class Meta:
+		model = Society
+		fields = '__all__'
 
-    def get_contributions(self):
-        return Society.objects.get_contributions(**self.data)
+	def save(self):
+		self.validated_data['creator'] = self.context['request'].user
+		super().save()
+
+	def get_contributions(self):
+		return Society.objects.get_contributions(**self.data)
 
 
 class SocietyUserSerializer(SocietySerializer):
-    users = UserContributionsSerializer(many=True, read_only=True)
+	users = UserContributionsSerializer(many=True, read_only=True)
 
-class SocietyDetailsSerializer(SocietyUserSerializer):
-    pass
+
+class SocietyTenureSerializer(serializers.ModelSerializer):
+	active_tenure = TenureSerializer(read_only=True)
+	class Meta:
+		model = Society
+		fields = '__all__'
+
+
